be67b13b82d3bc45b20e2edcdfa3ba672a82b7f7
GlaucioDias <fleevrl@gmail.com>
2026-09-12 23:40:26 -0300

    S3_25: Criando uma configuration e registrando um bean

---
+++ src/main/java/io/github/cursodsousa/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -0,0 +1,23 @@
+package io.github.cursodsousa.arquiteturaspring.montadora.api;
+
+import io.github.cursodsousa.arquiteturaspring.montadora.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/carros")
+public class TesteFabricaController {
+
+    @Autowired
+    private Motor motor;
+
+    @PostMapping("/ignicao")
+    public CarroStatus ligarCarro(@RequestBody Chave chave) {
+        Carro carro = new HondaHRV(motor);
+
+        return carro.darIgnicao(chave);
+    }
+}
